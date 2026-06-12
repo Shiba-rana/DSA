808ffb550beda414a23ed560330412f582a0988d
Silurana <silurana330@gmail.com>
2026-06-13 02:34:49 +0530

    Set()

---
+++ containsDuplicate.js
@@ -0,0 +1,14 @@
+function containsDuplicate(arr) {
+  let set = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(arr[i])) {
+      return true;
+    }
+    set.add(arr[i]);
+  }
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
