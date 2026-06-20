28b354fe7641582303186bb248fdca51d448e5d8
Silurana <silurana330@gmail.com>
2026-06-20 13:26:47 +0530

    Map()

---
+++ firstUnqCharStr.js
@@ -0,0 +1,14 @@
+function firstUniqChar(s) {
+    let map = new Map()
+
+    for(let ch of s) {
+        map.set(ch, (map.get(ch) || 0) + 1)
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        if(map.get(s[i]) === 1) return i
+    }
+    return -1
+}
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("Shiva"));
