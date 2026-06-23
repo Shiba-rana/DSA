394e809f80a031bac9320d49e31f9390a4788342
Silurana <silurana330@gmail.com>
2026-06-24 02:40:42 +0530

    Linked List Cycle

---
+++ Linked List/linkedListCycle.js
@@ -0,0 +1,14 @@
+function LinkedListCycle(head) {
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
+
+        if(slow === fast) {
+            return true
+        }
+    }
+    return false
+}
