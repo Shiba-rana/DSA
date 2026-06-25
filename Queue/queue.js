80d06abb46674678f5cdc255f1580de79a45d3c2
Silurana <silurana330@gmail.com>
2026-06-26 02:22:48 +0530

    Queue

---
+++ Queue/queue.js
@@ -0,0 +1,40 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(data) {
+    this.queue.push(data)
+  }
+
+  dequeue() {
+    return this.isEmpty() ? null : this.queue.shift();
+  }
+
+  front() {
+    return this.isEmpty() ? null : this.queue.at(0);
+  }
+
+  back() {
+    return this.isEmpty() ? null : this.queue.at(-1);
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
+
+const queue = new Queue()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+console.log(queue);
+console.log(queue.dequeue());
+console.log(queue);
+
+
+console.log(queue.front());
+console.log(queue.back());
+
