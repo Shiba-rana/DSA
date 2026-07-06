69a684c1bae9cc12d4dc1cb5062cf764ca0b8e56
Silurana <silurana330@gmail.com>
2026-07-06 21:03:16 +0530

    reverse array

--- revArray.js
+++ revArray.js
@@ -1,14 +1,27 @@
 //! 1
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-let temp = new Array(arr.length)
-let j = 0
-for(let i = arr.length -1; i >=0; i--) {
-    temp[j] = arr[i]
-    j++
+// let temp = new Array(arr.length)
+// let j = 0
+// for(let i = arr.length -1; i >=0; i--) {
+//     temp[j] = arr[i]
+//     j++
+// }
+
+// console.log(temp);
+
+//! 2 Different Way
+let array = [3, 4, 5, 6, 7]
+let i = 0, j = array.length -1 ;
+
+while(i != j) {
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    i++
+    j--
 }
 
-console.log(temp);
+console.log(array);
 
-//! 2
 
