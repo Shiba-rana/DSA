0b4b31386fcfbdba0d6faf0c95a49f8bba9b4c15
Silurana <silurana330@gmail.com>
2026-07-12 23:52:19 +0530

    4Sum

---
+++ 4Sum.js
@@ -0,0 +1,52 @@
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b);
+
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n - 2; j++) {
+
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] === nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+const nums = [1, 0, -1, 0, -2, 2];
+const target = 0;
+
+const ans = fourSum(nums, target);
+
+console.log("Quadruplets:");
+console.log(ans);
